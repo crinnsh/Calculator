2927c9bf30b6ddb8e2e8ff831850fdf542a6d543
crinnsh <117044606+crinnsh@users.noreply.github.com>
2022-11-23 00:17:25 +0100

    initial commit
    added original files for calculator.

---
+++ app.js
@@ -0,0 +1,38 @@
+log = console.log;
+
+let screen = document.getElementById("calculator-screen");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map(button => {
+    button.addEventListener("click", (b) => {
+        if (screen.innerText == "Noooooooo!") {
+            screen.innerText = "";
+        }
+
+        switch (b.target.innerText) {
+            case "CA":
+                screen.innerText = "";
+                break;
+
+            case "←":
+                if (screen.innerText) {
+                    screen.innerText = screen.innerText.slice(0, -1);
+                }
+                break;
+
+            case "=":
+                try {
+                    /* screen.innerText = eval(screen.innerText); */
+                    result = eval(screen.innerText);
+                    screen.innerText = result.toFixed(4);
+                } catch {
+                    screen.innerText = "Noooooooo!"
+                }
+                break;
+
+            default:
+                screen.innerText += b.target.innerText;
+        }
+    });
+});
